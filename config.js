7a4ceedc64e6c31957efb62ed46d7a6a5c97b8d7
idnettalent <94296386+idnettalent@users.noreply.github.com>
2021-11-14 22:00:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,11 +6,11 @@ const config = {
     trailingSlash: false,
   },
   header: {
-    logo: 'a',
-    logoLink: '',
+    logo: 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
+    logoLink: 'https://hasura.io/learn/',
     title:
-    "Sub Page No 2",
-    githubUrl: '',
+      "<a href='https://hasura.io/learn/'><img class='img-responsive' src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/learn-logo.svg' alt='Learn logo' /></a>",
+    githubUrl: 'https://github.com/hasura/gatsby-gitbook-boilerplate',
     helpUrl: '',
     tweetText: '',
     social: `<li>
